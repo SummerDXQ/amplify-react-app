2722f8c896e342fab7b97edfef351e93977fe9fd
summer.ding <summer.ding@ie.com.au>
2022-06-28 11:10:16 +0800

    Add Auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -18,9 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <AmplifySignOut/>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
